8280debff77b7919a73cfad898a9bd4a990d888d
JulianMayorga <mayorga.julian@gmail.com>
2016-09-15 10:42:52 -0300

    migrate tacos app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+// @flow
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from 'react-dom';
+
+import App from './components/App';
+
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+render(
+    <App />,
+    document.getElementById('root')
 );
